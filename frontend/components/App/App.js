3bb901d8fc6d6cc8c4f69ac34fdb0f5afc25e33f
Steven Klimek <steven.klimek7@yahoo.com>
2024-02-19 19:46:41 -0600

    posts

--- frontend/components/App/App.js
+++ frontend/components/App/App.js
@@ -5,14 +5,20 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import SearchBar from '../SearchBar/SearchBar'
+import Posts from '../Posts/Posts'
 // Import the dummyData
+import data from '../../data/dummy-data'
+
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts, setPosts] = useState(data);
+
 
   const likePost = postId => {
     /*
@@ -31,7 +37,9 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts likePost={likePost} posts={posts}/>
     </div>
   );
 };
